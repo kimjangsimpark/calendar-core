3f84c97d8890cafb3eef62bb803e56ac7ad2375f
simkoon <shk321233@naver.com>
2021-06-05 16:02:02 +0900

    add schedulbar component

---
+++ src/components/schedule/schedulebar.component.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Component, CustomElement } from '../../engines/component';
+import { CalendarService } from '../../service/calendar.service';
+import template from './schedulebar.component.html';
+import style from './schedulebar.component.scss';
+
+export interface Schedule {
+  id: string;
+  title: string;
+  content: string;
+  start: Date;
+  end: Date;
+  isAllDay: boolean;
+}
+
+@Component({
+  selector: 'kjsp-schedulebar',
+  template: template,
+  style: style,
+})
+export class SchedulebarComponent extends CustomElement {
+  public index: number;
+
+  public static get observedAttributes(): string[] {
+    return ['index'];
+  }
+
+  public constructor(private readonly calendarService: CalendarService) {
+    super();
+    this.calendarService.selectedYearAndMonth.subscribe((date) => {
+      console.log(date);
+    });
+  }
+
+  public attributeChangedCallback(
+    name: string,
+    oldValue: string,
+    newValue: string
+  ): void {
+    this.index = Number(newValue);
+  }
+}
