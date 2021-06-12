893c35fb1b649c636c9a213cc8f9968569bff89d
simkoon <shk321233@naver.com>
2021-06-12 15:58:54 +0900

    feat: set startDate endDate

--- src/components/schedule/schedule-week-container/schedule-week-container.component.ts
+++ src/components/schedule/schedule-week-container/schedule-week-container.component.ts
@@ -22,7 +22,22 @@ export class ScheduleWeekContainerComponent extends CustomElement {
   ) {
     super();
     this.calendarService.selectedYearAndMonth.subscribe((date) => {
-      console.log('서비스에서 받은 구독결과 인덱스 & 데이트', this.index, date);
+      const firstDay = this.calendarService.getSelectedFirstDay();
+      const axisDate = new Date(date.getTime());
+      console.log(' 데이트 :', date, axisDate);
+      axisDate.setDate(1);
+      axisDate.setDate(-firstDay + 1);
+      console.log('인덱스', this.index, axisDate);
+      this.startDay = new Date(axisDate.getTime());
+      this.startDay.setDate(
+        axisDate.getDate() + UnitsForSchedule.week * this.index
+      );
+      this.endDay = new Date(axisDate.getTime());
+      this.endDay.setDate(
+        axisDate.getDate() + UnitsForSchedule.week * (this.index + 1) - 1
+      );
+      console.log('스타트데이트:', this.startDay);
+      console.log('엔드데이트22:', this.endDay);
     });
     this.calendarService.selectedMonthSchedules.subscribe((scheduleList) => {
       console.log(scheduleList);
