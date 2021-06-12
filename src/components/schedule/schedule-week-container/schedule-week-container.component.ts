0182ccc8a9dea3a36d0e789078b9677fec49b3f1
simkoon <shk321233@naver.com>
2021-06-12 16:19:33 +0900

    feat: extract method

--- src/components/schedule/schedule-week-container/schedule-week-container.component.ts
+++ src/components/schedule/schedule-week-container/schedule-week-container.component.ts
@@ -21,26 +21,37 @@ export class ScheduleWeekContainerComponent extends CustomElement {
     private readonly index: number
   ) {
     super();
-    this.calendarService.selectedYearAndMonth.subscribe((date) => {
-      const firstDay = this.calendarService.getSelectedFirstDay();
-      const axisDate = new Date(date.getTime());
-      console.log(' 데이트 :', date, axisDate);
-      axisDate.setDate(1);
-      axisDate.setDate(-firstDay + 1);
-      console.log('인덱스', this.index, axisDate);
-      this.startDay = new Date(axisDate.getTime());
-      this.startDay.setDate(
-        axisDate.getDate() + UnitsForSchedule.week * this.index
-      );
-      this.endDay = new Date(axisDate.getTime());
-      this.endDay.setDate(
-        axisDate.getDate() + UnitsForSchedule.week * (this.index + 1) - 1
-      );
-      console.log('스타트데이트:', this.startDay);
-      console.log('엔드데이트22:', this.endDay);
-    });
+    this.calendarService.selectedYearAndMonth.subscribe(this.getSubscriber());
     this.calendarService.selectedMonthSchedules.subscribe((scheduleList) => {
       console.log(scheduleList);
     });
+  }
+
+  private getSubscriber(): (date: Date) => void {
+    const getSelectedFirstDay = (): number => {
+      return this.calendarService.getSelectedFirstDay();
+    };
+
+    const getAxisDate = (date: Date) => {
+      const axisDate = new Date(date.getTime());
+      axisDate.setDate(1);
+      axisDate.setDate(-getSelectedFirstDay.call(this) + 1);
+      return axisDate;
+    };
+
+    return (date) => {
+      this.startDay = new Date(getAxisDate(date).getTime());
+      this.startDay.setDate(
+        getAxisDate(date).getDate() + UnitsForSchedule.week * this.index
+      );
+      this.endDay = new Date(getAxisDate(date).getTime());
+      this.endDay.setDate(
+        getAxisDate(date).getDate() +
+          UnitsForSchedule.week * (this.index + 1) -
+          1
+      );
+      console.log('스타트데이트:', this.startDay);
+      console.log('엔드데이트22:', this.endDay);
+    };
   }
 }
