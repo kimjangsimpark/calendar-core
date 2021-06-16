cac787b38ceb59fe792246cb2919294c5688a96a
winetree94 <winetree94@outlook.com>
2021-06-16 21:54:51 +0900

    feat: separate dialog component

--- src/components/dialogs/add-schedule/add-schedule.component.ts
+++ src/components/dialogs/add-schedule/add-schedule.component.ts
@@ -1,7 +1,15 @@
 import { Component, CustomElement } from '../../../engines/component';
+import { DialogComponent } from '../../extendables/dialog.component';
 import { DialogTargetAxis, OverlayComponent } from '../../overlay/overlay.component';
 import template from './add-schedule.component.html';
 import style from './add-schedule.component.scss';
+
+enum Position {
+  RIGHT,
+  BOTTOM,
+  LEFT,
+  TOP,
+}
 
 enum X {
   RIGHT,
@@ -20,66 +28,13 @@ enum Y {
   template: template,
   style: style,
 })
-export class AddScheduleComponent extends CustomElement {
-
-  public xMode: X;
-  public yMode: Y;
+export class AddScheduleComponent extends DialogComponent {
 
   public constructor(
-    private readonly overlayComponent: OverlayComponent,
-    private readonly parentAxis: DialogTargetAxis
+    public readonly overlayComponent: OverlayComponent,
+    public readonly parentAxis: DialogTargetAxis
   ) {
-    super();
-    this.overlayComponent.clear();
-    this.overlayComponent.shadowRoot.append(this);
-    this.shadowRoot.querySelector('#close').addEventListener('click', this.onCloseClicked.bind(this));
-    this.setX();
-    this.setY();
-  }
-  
-  public setX(): void {
-    const rect = this.getBoundingClientRect();
-    const maxWidth = this.overlayComponent.getMaxWidth();
-    const maxHeight = this.overlayComponent.getMaxHeight();
-    if (
-      this.parentAxis.x + this.parentAxis.width + rect.width < maxWidth
-    ) {
-      this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
-      this.xMode = X.RIGHT;
-    } else if (
-      this.parentAxis.x + this.parentAxis.width + ((rect.width - this.parentAxis.width) / 2) < maxWidth
-    ) {
-      this.style.left = `${this.parentAxis.x - ((rect.width - this.parentAxis.width) / 2)}px`;
-      this.xMode = X.MIDDLE;
-    } else if (
-      this.parentAxis.x - rect.width < this.parentAxis.x
-    ) {
-      this.style.left = `${this.parentAxis.x - rect.width}px`;
-      this.xMode = X.LEFT;
-    } else {
-      throw new Error('Unsupported position');
-    }
-  }
-
-  public setY(): void {
-    const rect = this.getBoundingClientRect();
-    const maxHeight = this.overlayComponent.getMaxHeight();
-    if (
-      this.parentAxis.y + this.parentAxis.height + ((rect.height - this.parentAxis.height) / 2) <= maxHeight &&
-      this.xMode !== X.MIDDLE
-    ) {
-      this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
-      this.yMode = Y.MIDDLE;
-    } else {
-      if (
-        this.parentAxis.y + this.parentAxis.height + rect.height <= maxHeight
-      ) {
-        this.style.top = `${this.parentAxis.y + this.parentAxis.height}px`;
-        this.yMode = Y.BOTTOM;
-      } else {
-        this.style.top = `${this.parentAxis.y - rect.height}px`;
-      }
-    }
+    super(overlayComponent, parentAxis);
   }
 
   public onCloseClicked(): void {
---
+++ src/components/extendables/dialog.component.ts
@@ -0,0 +1,88 @@
+import { CustomElement } from '../../engines/component';
+import { DialogTargetAxis, OverlayComponent } from '../overlay/overlay.component';
+
+enum Position {
+  RIGHT,
+  BOTTOM,
+  LEFT,
+  TOP,
+}
+
+enum X {
+  RIGHT,
+  MIDDLE,
+  LEFT,
+}
+
+enum Y {
+  TOP,
+  MIDDLE,
+  BOTTOM,
+}
+
+export class DialogComponent extends CustomElement {
+
+  public xMode: X;
+  public yMode: Y;
+
+  protected constructor(
+    public readonly overlayComponent: OverlayComponent,
+    public readonly parentAxis: DialogTargetAxis
+  ) {
+    super();
+    this.overlayComponent.clear();
+    this.overlayComponent.shadowRoot.append(this);
+    const rect = this.getBoundingClientRect();
+    this.style.position = 'absolute';
+    this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
+    this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
+    // this.setX();
+    // this.setY();
+  }
+
+  // public setX(): void {
+  //   const rect = this.getBoundingClientRect();
+  //   const maxWidth = this.overlayComponent.getMaxWidth();
+  //   const maxHeight = this.overlayComponent.getMaxHeight();
+  //   if (
+  //     this.parentAxis.x + this.parentAxis.width + rect.width < maxWidth
+  //   ) {
+  //     this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
+  //     this.xMode = X.RIGHT;
+  //   } else if (
+  //     this.parentAxis.x + this.parentAxis.width + ((rect.width - this.parentAxis.width) / 2) < maxWidth
+  //   ) {
+  //     this.style.left = `${this.parentAxis.x - ((rect.width - this.parentAxis.width) / 2)}px`;
+  //     this.xMode = X.MIDDLE;
+  //   } else if (
+  //     this.parentAxis.x - rect.width < this.parentAxis.x
+  //   ) {
+  //     this.style.left = `${this.parentAxis.x - rect.width}px`;
+  //     this.xMode = X.LEFT;
+  //   } else {
+  //     throw new Error('Unsupported position');
+  //   }
+  // }
+
+  // public setY(): void {
+  //   const rect = this.getBoundingClientRect();
+  //   const maxHeight = this.overlayComponent.getMaxHeight();
+  //   if (
+  //     this.parentAxis.y + this.parentAxis.height + ((rect.height - this.parentAxis.height) / 2) <= maxHeight &&
+  //     this.xMode !== X.MIDDLE
+  //   ) {
+  //     this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
+  //     this.yMode = Y.MIDDLE;
+  //   } else {
+  //     if (
+  //       this.parentAxis.y + this.parentAxis.height + rect.height <= maxHeight
+  //     ) {
+  //       this.style.top = `${this.parentAxis.y + this.parentAxis.height}px`;
+  //       this.yMode = Y.BOTTOM;
+  //     } else {
+  //       this.style.top = `${this.parentAxis.y - rect.height}px`;
+  //     }
+  //   }
+  // }
+
+}
