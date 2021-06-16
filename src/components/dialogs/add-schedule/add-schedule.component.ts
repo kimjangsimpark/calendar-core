b54da01d98d3e10eaa629bf4f9f87ac9adce337a
winetree94 <winetree94@outlook.com>
2021-06-16 21:33:37 +0900

    feat: support automatic adjucst

--- src/components/dialogs/add-schedule/add-schedule.component.ts
+++ src/components/dialogs/add-schedule/add-schedule.component.ts
@@ -3,14 +3,17 @@ import { DialogTargetAxis, OverlayComponent } from '../../overlay/overlay.compon
 import template from './add-schedule.component.html';
 import style from './add-schedule.component.scss';
 
-enum Position {
-  TOP,
+enum X {
   RIGHT,
-  BOTTOM,
+  MIDDLE,
   LEFT,
 }
 
-const ExpectedOrder: Position[] = [Position.TOP, Position.RIGHT, Position.BOTTOM, Position.LEFT];
+enum Y {
+  TOP,
+  MIDDLE,
+  BOTTOM,
+}
 
 @Component({
   selector: 'kjsp-add-schedule',
@@ -19,6 +22,9 @@ const ExpectedOrder: Position[] = [Position.TOP, Position.RIGHT, Position.BOTTOM
 })
 export class AddScheduleComponent extends CustomElement {
 
+  public xMode: X;
+  public yMode: Y;
+
   public constructor(
     private readonly overlayComponent: OverlayComponent,
     private readonly parentAxis: DialogTargetAxis
@@ -26,32 +32,57 @@ export class AddScheduleComponent extends CustomElement {
     super();
     this.overlayComponent.clear();
     this.overlayComponent.shadowRoot.append(this);
-    this.getRendablePosition();
+    this.shadowRoot.querySelector('#close').addEventListener('click', this.onCloseClicked.bind(this));
+    this.setX();
+    this.setY();
   }
   
-  public getRendablePosition(): void {
+  public setX(): void {
     const rect = this.getBoundingClientRect();
-
     const maxWidth = this.overlayComponent.getMaxWidth();
     const maxHeight = this.overlayComponent.getMaxHeight();
-
-    // 우하좌상 우선순위 x 축 정의
     if (
       this.parentAxis.x + this.parentAxis.width + rect.width < maxWidth
     ) {
       this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
+      this.xMode = X.RIGHT;
     } else if (
       this.parentAxis.x + this.parentAxis.width + ((rect.width - this.parentAxis.width) / 2) < maxWidth
     ) {
       this.style.left = `${this.parentAxis.x - ((rect.width - this.parentAxis.width) / 2)}px`;
+      this.xMode = X.MIDDLE;
     } else if (
       this.parentAxis.x - rect.width < this.parentAxis.x
     ) {
       this.style.left = `${this.parentAxis.x - rect.width}px`;
+      this.xMode = X.LEFT;
     } else {
       throw new Error('Unsupported position');
     }
+  }
 
-    // y 축 보정 실행
+  public setY(): void {
+    const rect = this.getBoundingClientRect();
+    const maxHeight = this.overlayComponent.getMaxHeight();
+    if (
+      this.parentAxis.y + this.parentAxis.height + ((rect.height - this.parentAxis.height) / 2) <= maxHeight &&
+      this.xMode !== X.MIDDLE
+    ) {
+      this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
+      this.yMode = Y.MIDDLE;
+    } else {
+      if (
+        this.parentAxis.y + this.parentAxis.height + rect.height <= maxHeight
+      ) {
+        this.style.top = `${this.parentAxis.y + this.parentAxis.height}px`;
+        this.yMode = Y.BOTTOM;
+      } else {
+        this.style.top = `${this.parentAxis.y - rect.height}px`;
+      }
+    }
+  }
+
+  public onCloseClicked(): void {
+    this.overlayComponent.clear();
   }
 }
