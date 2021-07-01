6bc2536d4fd4892bd92060ac4285804f5d9c3b2c
simkoon <shk321233@naver.com>
2021-07-01 16:33:33 +0900

    feat: add min.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,90 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 const PATH = {
-  SRC: path.resolve(__dirname, './src'),
+    SRC: path.resolve(__dirname, "./src"),
 };
 
 function publicPathResolver(resourcePath, context) {
-  const relativePath = path.relative(path.dirname(resourcePath), context);
-  const posixPath = relativePath.split(path.win32.sep).join(path.posix.sep);
-  return posixPath + '/';
+    const relativePath = path.relative(path.dirname(resourcePath), context);
+    const posixPath = relativePath.split(path.win32.sep).join(path.posix.sep);
+    return posixPath + "/";
 }
 
 module.exports = {
-  mode: 'development',
-  entry: prod ? './src/index.prod.ts' : './src/index.dev.ts',
-  module: {
-    rules: [
-      {
-        test: /\.(ts)?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
+    mode: "development",
+    entry: prod ? "./src/index.prod.ts" : "./src/index.dev.ts",
+    module: {
+        rules: [
+            {
+                test: /\.(ts)?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(html)?$/,
+                exclude: [
+                    path.resolve(PATH.SRC, "./index.dev.html"),
+                    path.resolve(PATH.SRC, "./index.prod.html"),
+                ],
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: prod,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(scss)?$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
         ],
-      },
-      {
-        test: /\.(html)?$/,
-        exclude: [
-          path.resolve(PATH.SRC, './index.dev.html'),
-          path.resolve(PATH.SRC, './index.prod.html'),
-        ],
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: prod,
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(scss)?$/,
-        use: [
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ],
-      },
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    devServer: {
+        port: 9010,
+        host: "0.0.0.0",
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: "body",
+            template: "src/index.dev.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "kjsp-calendar-core.min.css",
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  devServer: {
-    port: 9010,
-    host: '0.0.0.0',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: 'src/index.dev.html',
-    }),
-  ],
-  output: {
-    library: 'kjsp-calendar-core',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, './dist'),
-    filename: 'kjsp-calendar-core.min.js'
-  },
-  devtool: 'inline-source-map',
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-  },
+    output: {
+        library: "kjsp-calendar-core",
+        libraryTarget: "umd",
+        path: path.resolve(__dirname, "./dist"),
+        filename: "kjsp-calendar-core.min.js",
+    },
+    devtool: "inline-source-map",
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
 };
