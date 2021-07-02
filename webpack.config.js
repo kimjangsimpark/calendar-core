a0cbf8ff6dbd6159f814865070fd41b0a438cfbb
simkoon <shk321233@naver.com>
2021-07-02 17:36:13 +0900

    npm test

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,22 @@
+const DeclarationFilesPlugin = require("@ns0m/witty-webpack-declaration-files");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const DeclarationFilesPlugin = require("@ns0m/witty-webpack-declaration-files");
-var glob = require("glob");
-
 const prod = process.env.NODE_ENV === "production";
-
 const PATH = {
     SRC: path.resolve(__dirname, "./src"),
 };
-
 function publicPathResolver(resourcePath, context) {
     const relativePath = path.relative(path.dirname(resourcePath), context);
     const posixPath = relativePath.split(path.win32.sep).join(path.posix.sep);
     return posixPath + "/";
 }
-const entryFiles = glob.sync('./src/**/*.ts').reduce((previousValue, currentValue, currentIndex, array) => {
-    console.log(currentValue)
-
-    if(currentValue.includes('.d.ts')) return {...previousValue}
-    return typeof previousValue === 'string' ?
-      {
-        [path.basename(previousValue, path.extname(previousValue))]: previousValue,
-        [path.basename(currentValue, path.extname(currentValue))]: currentValue
-      }
-      :
-      { ...previousValue, [path.basename(currentValue, path.extname(currentValue))]: currentValue }
-  })
-
 module.exports = {
     mode: "development",
-    // entry: prod ? glob.sync("./src/**/*.ts") : "./src/index.dev.ts",
-    // entry: prod ? entryFiles : "./src/index.dev.ts",
-    entry: prod ? './src/index.prod.ts' : "./src/index.dev.ts",
+    entry: prod ? "./src/index.prod.ts" : "./src/index.dev.ts",
     module: {
         rules: [
             {
@@ -82,14 +63,16 @@ module.exports = {
         host: "0.0.0.0",
     },
     plugins: [
-        new DeclarationFilesPlugin({
-            merge: true,
-            filename: 'index.prod.d.ts',
+        new CleanWebpackPlugin({
+            // dangerouslyAllowCleanPatternsOutsideProject: true,
         }),
-        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             inject: "body",
             template: "src/index.dev.html",
+        }),
+        new DeclarationFilesPlugin({
+            merge: true,
+            filename: "index.prod.d.ts",
         }),
     ],
     output: {
@@ -100,10 +83,7 @@ module.exports = {
     },
     devtool: "inline-source-map",
     optimization: {
-        // minimize: false,
-        // minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-        // splitChunks: {
-        //     chunks: 'all',
-        //   },
+        minimize: true,
+        minimizer: [new TerserPlugin()],
     },
 };
