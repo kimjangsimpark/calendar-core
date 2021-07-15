ad7ab5f3ebd636b5f535504b5608efd41a58a133
winetree94 <winetree94@outlook.com>
2021-07-15 21:36:02 +0900

    feat: enable auto layout position fixer

--- src/components/extendables/dialog.component.ts
+++ src/components/extendables/dialog.component.ts
@@ -36,53 +36,53 @@ export class DialogComponent extends CustomElement {
     this.style.position = 'absolute';
     this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
     this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
-    // this.setX();
-    // this.setY();
+    this.setX();
+    this.setY();
   }
 
-  // public setX(): void {
-  //   const rect = this.getBoundingClientRect();
-  //   const maxWidth = this.overlayComponent.getMaxWidth();
-  //   const maxHeight = this.overlayComponent.getMaxHeight();
-  //   if (
-  //     this.parentAxis.x + this.parentAxis.width + rect.width < maxWidth
-  //   ) {
-  //     this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
-  //     this.xMode = X.RIGHT;
-  //   } else if (
-  //     this.parentAxis.x + this.parentAxis.width + ((rect.width - this.parentAxis.width) / 2) < maxWidth
-  //   ) {
-  //     this.style.left = `${this.parentAxis.x - ((rect.width - this.parentAxis.width) / 2)}px`;
-  //     this.xMode = X.MIDDLE;
-  //   } else if (
-  //     this.parentAxis.x - rect.width < this.parentAxis.x
-  //   ) {
-  //     this.style.left = `${this.parentAxis.x - rect.width}px`;
-  //     this.xMode = X.LEFT;
-  //   } else {
-  //     throw new Error('Unsupported position');
-  //   }
-  // }
+  public setX(): void {
+    const rect = this.getBoundingClientRect();
+    const maxWidth = this.overlayComponent.getMaxWidth();
+    const maxHeight = this.overlayComponent.getMaxHeight();
+    if (
+      this.parentAxis.x + this.parentAxis.width + rect.width < maxWidth
+    ) {
+      this.style.left = `${this.parentAxis.x + this.parentAxis.width}px`;
+      this.xMode = X.RIGHT;
+    } else if (
+      this.parentAxis.x + this.parentAxis.width + ((rect.width - this.parentAxis.width) / 2) < maxWidth
+    ) {
+      this.style.left = `${this.parentAxis.x - ((rect.width - this.parentAxis.width) / 2)}px`;
+      this.xMode = X.MIDDLE;
+    } else if (
+      this.parentAxis.x - rect.width < this.parentAxis.x
+    ) {
+      this.style.left = `${this.parentAxis.x - rect.width}px`;
+      this.xMode = X.LEFT;
+    } else {
+      throw new Error('Unsupported position');
+    }
+  }
 
-  // public setY(): void {
-  //   const rect = this.getBoundingClientRect();
-  //   const maxHeight = this.overlayComponent.getMaxHeight();
-  //   if (
-  //     this.parentAxis.y + this.parentAxis.height + ((rect.height - this.parentAxis.height) / 2) <= maxHeight &&
-  //     this.xMode !== X.MIDDLE
-  //   ) {
-  //     this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
-  //     this.yMode = Y.MIDDLE;
-  //   } else {
-  //     if (
-  //       this.parentAxis.y + this.parentAxis.height + rect.height <= maxHeight
-  //     ) {
-  //       this.style.top = `${this.parentAxis.y + this.parentAxis.height}px`;
-  //       this.yMode = Y.BOTTOM;
-  //     } else {
-  //       this.style.top = `${this.parentAxis.y - rect.height}px`;
-  //     }
-  //   }
-  // }
+  public setY(): void {
+    const rect = this.getBoundingClientRect();
+    const maxHeight = this.overlayComponent.getMaxHeight();
+    if (
+      this.parentAxis.y + this.parentAxis.height + ((rect.height - this.parentAxis.height) / 2) <= maxHeight &&
+      this.xMode !== X.MIDDLE
+    ) {
+      this.style.top = `${this.parentAxis.y - ((rect.height - this.parentAxis.height) / 2)}px`;
+      this.yMode = Y.MIDDLE;
+    } else {
+      if (
+        this.parentAxis.y + this.parentAxis.height + rect.height <= maxHeight
+      ) {
+        this.style.top = `${this.parentAxis.y + this.parentAxis.height}px`;
+        this.yMode = Y.BOTTOM;
+      } else {
+        this.style.top = `${this.parentAxis.y - rect.height}px`;
+      }
+    }
+  }
 
 }
